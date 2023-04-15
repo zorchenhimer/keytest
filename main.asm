912d5d01639e2e36fb6a6bfd9acad77ef047ee85
Zorchenhimer <zorchenhimer@gmail.com>
2023-04-14 22:59:08 -0400

    Start drawing some stuff
    - Draw menu
    - Make menu functional
    - Start drawing controllers screen

--- main.asm
+++ main.asm
@@ -18,6 +18,12 @@ Sleeping: .res 1
 TmpX: .res 1
 TmpY: .res 1
 
+btnX: .res 1
+btnY: .res 1
+
+AddressPointer: .res 2
+MenuSelection: .res 1
+
 .segment "OAM"
 SpriteZero: .res 4
 Sprites: .res (64*4)-4
@@ -103,6 +109,45 @@ ReadControllers:
     bne @player2
     rts
 
+ButtonPressedP1:
+    sta btnX
+    and controller1
+    sta btnY
+
+    lda controller1_old
+    and btnX
+
+    cmp btnY
+    bne btnPress_stb
+
+    ; no button change
+    rts
+
+ButtonPressedP2:
+    sta btnX
+    and controller2
+    sta btnY
+
+    lda controller2_old
+    and btnX
+
+    cmp btnY
+    bne btnPress_stb
+
+    ; no button change
+    rts
+
+btnPress_stb:
+    ; button released
+    lda btnY
+    bne btnPress_stc
+    rts
+
+btnPress_stc:
+    ; button pressed
+    lda #1
+    rts
+
 ReadKeyboard:
     rts
 
@@ -118,9 +163,58 @@ WaitForNMI:
 
 ; Menu to select what type of input device
 DrawMenu:
-    rts
+    lda #$21
+    sta AddressPointer+1
+    sta $2006
+    lda #$69
+    sta AddressPointer+0
+    sta $2006
+    sta TmpX
 
-DrawControllers:
+    ldy #3
+    ldx #0
+@loop:
+    lda MenuItems, x
+    beq @next
+    inx
+    sta $2007
+    jmp @loop
+
+@next:
+    inx
+    dey
+    beq @done
+    clc
+    lda AddressPointer+0
+    adc #$40
+    sta AddressPointer+0
+
+    lda AddressPointer+1
+    adc #0
+    sta AddressPointer+1
+    sta $2006
+
+    lda AddressPointer+0
+    sta $2006
+    jmp @loop
+
+@done:
+
+    ;sprite stuff
+    lda #0
+    sta MenuSelection
+    sta SpriteZero+2
+    asl a
+    tax
+
+    lda #$11
+    sta SpriteZero+1
+
+    lda MenuRows, x
+    sta SpriteZero+0
+    lda MenuRows+1, x
+    sta SpriteZero+3
+
     rts
 
 DrawKeyboard:
@@ -147,7 +241,7 @@ ClearScreen:
     sta $2007
     dey
     bne :-
-    ldy #32
+    ldy #8
     dex
     bne :-
 
@@ -214,13 +308,184 @@ RESET:
     lda #$02
     sta $4014
 
-    jmp WaitForNMI
-    jmp WaitForNMI
+    lda #$3F
+    sta $2006
+    lda #$00
+    sta $2006
+
+    ldx #0
+:
+    lda Palettes, x
+    sta $2007
+    inx
+    cpx #32
+    bne :-
+
+    lda #%1000_0000
+    sta $2000
+
+    ;lda #%0001_1110
+    lda #0
+    sta $2001
+
+    jsr WaitForNMI
+    jsr WaitForNMI
 
     ; TODO: palatte
 
     jsr ClearScreen
     jsr DrawMenu
 
-Frame:
-    jmp Frame
+    jsr WaitForNMI
+
+    lda #%0001_1110
+    sta $2001
+
+Frame_Menu:
+    jsr ReadControllers
+
+    lda #BUTTON_DOWN
+    jsr ButtonPressedP1
+    beq :+
+    inc MenuSelection
+    lda MenuSelection
+    cmp #3
+    bcc :+
+    lda #0  ; handle overflow
+    sta MenuSelection
+:
+    lda #BUTTON_UP
+    jsr ButtonPressedP1
+    beq :+
+    dec MenuSelection
+    lda MenuSelection
+    bpl :+
+    lda #2
+    sta MenuSelection
+:
+
+    lda #BUTTON_A
+    jsr ButtonPressedP1
+    beq :+
+    jsr WaitForNMI
+    lda MenuSelection
+    asl a
+    tax
+    lda MenuDestinations+0, x
+    sta AddressPointer+0
+    lda MenuDestinations+1, x
+    sta AddressPointer+1
+    jmp (AddressPointer)
+:
+
+    lda MenuSelection
+    asl a
+    tax
+
+    lda MenuRows, x
+    sta SpriteZero+0
+    lda MenuRows+1, x
+    sta SpriteZero+3
+
+    jmp Frame_Menu
+
+ControllerLabels:
+    .asciiz "Player 1"
+    .asciiz "Player 2"
+    .asciiz "Player 3"
+    .asciiz "Player 4"
+
+ControllerLabelAddrs:
+    .word $2084
+    .word $2090
+    .word $2204
+    .word $2210
+
+ControllerTiles:
+    ;.byte 
+
+DrawController:
+    ; Start address in AddressPointer
+
+Init_Controllers:
+
+    lda #%1000_0000
+    sta $2000
+
+    lda #0
+    sta $2001
+
+    jsr ClearScreen
+
+    lda #0
+    sta SpriteZero+1
+
+    ldx #0
+    ldy #0
+@txtStart:
+    lda ControllerLabelAddrs+1, x
+    sta $2006
+    lda ControllerLabelAddrs+0, x
+    sta $2006
+
+@txtLoop:
+    lda ControllerLabels, y
+    beq @next
+    iny
+    sta $2007
+    jmp @txtLoop
+
+@next:
+    iny
+    inx
+    inx
+    cpx #7
+    bcc @txtStart
+
+    jsr WaitForNMI
+    lda #%0001_1110
+    sta $2001
+
+Frame_Controllers:
+    jsr ReadControllers
+
+    jsr WaitForNMI
+    jmp Frame_Controllers
+
+Init_Keyboard:
+    brk
+    jmp Init_Keyboard
+
+Init_Feet:
+    brk
+    jmp Init_Feet
+
+MenuItems:
+    .asciiz "Controllers"
+    .asciiz "Keyboard"
+    .asciiz "Family Trainer"
+
+MenuRows:
+    ;      Y, X
+    .byte 87,  56
+    .byte 103, 56
+    .byte 119, 56
+
+MenuDestinations:
+    .word Init_Controllers
+    .word Init_Keyboard
+    .word Init_Feet
+
+Palettes:
+    ; BG
+    ; Sprite
+    .byte $0F, $20, $10, $00
+    .byte $0F, $20, $10, $00
+    .byte $0F, $20, $10, $00
+    .byte $0F, $20, $10, $00
+
+    ; Sprite
+    .byte $0F, $20, $10, $00
+    .byte $0F, $20, $10, $00
+    .byte $0F, $20, $10, $00
+    .byte $0F, $20, $10, $00
